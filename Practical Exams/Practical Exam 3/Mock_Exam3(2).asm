b55877bbfcddf306a9d50c7e3a051bb56aaf3a16
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-24 23:57:39 +0900

    Update Mock_Exam3(2).asm

--- Practical Exams/Practical Exam 3/Mock_Exam3(2).asm
+++ Practical Exams/Practical Exam 3/Mock_Exam3(2).asm
@@ -1,13 +1,10 @@
-; Name: [Doyun GU]
-; UID:  [11095970]
-
     processor 18F8722
     config OSC=HS, WDT=OFF, LVP=OFF
     radix decimal
     org 0x00
     
 LATA equ 0xF89 
-TRISA equ 0xF92 
+TRISA equ 0xF92
 PORTA equ 0xF80
 LATB equ 0xF8A 
 TRISB equ 0xF93 
@@ -15,7 +12,7 @@ PORTB equ 0xF81
 LATC equ 0xF8B 
 TRISC equ 0xF94 
 PORTC equ 0xF82
-LATF equ 0xF8E 
+LATF equ 0xF8E
 TRISF equ 0xF97 
 PORTF equ 0xF85
 LATH equ 0xF90 
@@ -24,125 +21,98 @@ PORTH equ 0xF87
 LATJ equ 0xF91 
 TRISJ equ 0xF9A 
 PORTJ equ 0xF88
-ADCON1 equ 0xFC1
+ADCON1 equ 0xFC1      
+;variables:
+DelayCounter1 equ 0x00
+DelayC2 equ 0x01
+DelayC3 equ 0x02
+Switches equ 0x03
 
+;setup TRIS - non used pins set to input to protect pins
 
-; THE TRISTATES
-    MOVLW B'00100000'    ; PB1
-    MOVWF TRISJ
-   
-    MOVLW B'11111100'    ; TRANSISTOR Q2
-    MOVWF TRISH
-   
-    MOVLW B'11101111'    ; TRANSISTOR Q3
-    MOVWF TRISA
-   
-    MOVLW B'00011100'    ; SWITCHES 1-3 RC2-4
-    MOVWF TRISC
-   
-    MOVLW B'00000000'    ; LEDs
+    MOVLW B'00000000';for anything all output
     MOVWF TRISF
+ 
+    MOVLW B'11111111' ; Anything all input
+    MOVWF TRISC
+    MOVWF TRISB
+    MOVWF TRISJ
+ 
+    MOVLW B'11110000'; First pins for transistors, last for switches
+    MOVWF TRISH
+ 
+    MOVLW B'11101111';For controlling LED transistor - RA4 
+    MOVWF TRISA
     
-    MOVLW 0X0F           ; ANALOGUE TO DIGITAL INPUT
-    MOVWF ADCON1
-   
-L_MAIN
-    NOP
-    ;START OF PART 1
-    MOVLW B'11011111'    ; SETTING THE VALUE OF PB1 TO LOW SO IT ACTIVATES WHEN PUSHED DOWN
-    MOVWF LATJ
-   
-    MOVF PORTJ, W        ; READING THE VALUE OF PB1 AND STORING INPUT
-    MOVWF 0X00
-   
-    BTFSC 0X00, 5        ; IF STATEMENT FOR CODE TO EXECUTE IF BIT 5 IS 0
-    BRA Conduction_not_filled
-    
-    MOVLW B'10000101'    ; MASKING THE INPUT FROM PB1 TO DISPLAY A "U" WHEN PUSHED
-    ANDWF 0X00
-    
-    MOVLW B'00000000'    ; TURNING OFF TRANSISTOR Q3
-    MOVWF LATA
+    MOVLB 4; Set BSR
+
+Main 
+
+    CALL GetSwitch; check for pushbutton
     
     MOVLW B'11111111'
-    MOVWF LATF           ; MASKING SURE THAT ALL SEGMENTS ARE OFF
+    MOVWF LATF;set data - all LEDs will be on
     
-    MOVLW B'11111101'    ; SETTING VALUE OF TRANSISTOR Q2 TO LOW TO TURN ON
+    MOVLW B'000000011';both segments off - does not need changing.
     MOVWF LATH
     
-    MOVF 0X00, W         ; BRINGING THE VALUE FROM PB1 TO LEDs
-    MOVWF LATF
+    MOVLW B'00010000';leds on
+    MOVWF LATA; RA4 set to high
     
-    MOVLW B'11111111'    ; SETTING TRANSISTOR Q2 AS OUTPUT TO TURN OFF
-    MOVWF LATH
+    BTFSS Switches, 5;if switch pressed
+    CALL DelaySub400ms;400ms
     
-    MOVLW B'11111111'    ; TURNING OFF THE PB1
-    MOVWF LATJ
+    BTFSC Switches, 5; if switch not pressed
+    CALL DelaySub40ms;delay 40ms
     
-Conduction_not_filled    ; CODE MOVES HERE WHEN THE PB1 IS NOT PUSHED
+    MOVLW B'00000000';leds off
+    MOVWF LATA; RA4 set to low
     
-    ; START OF PART 2
-    MOVLW B'11111111'    ; SETTING TRANSISTOR Q2 AS OUTPUT TO TURN OFF
-    MOVWF LATH
+    BTFSS Switches, 5;if switch pressed
+    CALL DelaySub400ms;400ms
     
-    MOVLW B'00000000'    ; CLEARING WHATEVER IN F
-    MOVWF LATF
+    BTFSC Switches, 5; if switch not pressed
+    CALL DelaySub40ms;delay 40ms
     
-    MOVLW B'00010000'    ; TURNING ON TRANSISTOR Q3 BY SETTING IT TO HIGH
-    MOVWF LATA
+    BRA Main; loop to start
     
-    MOVF PORTC, W        ; MOVING THE INPUT OF SWITCHES TO WREG 
-    MOVWF 0X02           ; SHIFTING THE VALUE OF SWITCHES 1-3 TO THE 2 TO THE RIGHT TO BE DISPLAYED AS THE RIGHTMOST LEDs
-    
-    RRNCF 0X02, F
-    RRNCF 0X02, W
-    ANDLW B'00000111'    ; MAKING THE INPUT OF THE SWITCHES SO ONLY 1-3 ARE INPUTS
-    MOVWF LATF
-    
-    ; END OF PART 2
-    ; START OF PART 3 AND PART 4: SAVING THAT VALUE INTO BANK1
-    
-    MOVLB 0X1             ; ALLOWS ACCESS TO BANK 1
-    MOVF PORTH, W         ; READING AVLUE FROM SWITCHES 8-5
-    ANDLW B'11110000'     ; MASKING THESE VALUES TO ONLY READ THESE 4 SWITCHES
-    MOVWF 0X01, 1
-    IORWF 0X02, F         ; PUTTING THE VALUE FROM SWTICHES INTO ASSIGNED MEMORY
-    MOVWF LATF            ; DISPLAY THE RESULT ON THE LEDs
+Delay_Sub_200us
+    MOVLW 71
+    MOVWF DelayCounter1,1;set counter, BSR used
+
+InnerLoop1
+    NOP;waste a load of time
     NOP
+    NOP
+    NOP
+    DECF DelayCounter1,1,1 ; decrease counter, BSR used 
+    BNZ InnerLoop1;if counter not yet zero, loop
+    RETURN
     
-    ; PART 5
-    MOVF 0X101, W
+DelaySub40ms
+    MOVLW 200;40ms/200us = 200 iterations
+    MOVWF DelayC2
+
+InnerLoop2
+    CALL Delay_Sub_200us;call 200us delay
+    DECF DelayC2; decrease counter
+    BNZ InnerLoop2;if counter not yet zero, loop
+    RETURN   
     
-    ; CHECK THAT NUMBER IS POSITIVE
+DelaySub400ms
+    MOVLW 10;overflows if using 2000x200us, so instead use 10x40ms
+    MOVWF DelayC3
+
+InnerLoop3
+    CALL DelaySub40ms;call 40ms delay 
+    DECF DelayC3; decrease counter
+    BNZ InnerLoop3;if counter not yet zero, loop
+    RETURN
     
-    BTFSC 0X101, 7         ; IF STATEMENT FOR CODE TO EXECUTE IF BIT 7 IS 0
-    BRA NUMBER_IS_NEGATIVE
+GetSwitch; Reads PB1
+    MOVF PORTJ, W; read in PortJ for PB1
+    ANDLW B'00100000';only J5 needed
+    MOVWF Switches
+    RETURN
     
-    ;ROTATE THE NUMBER 4 TIMES
-    RRNCF 0X101, F
-    RRNCF 0X101, F
-    RRNCF 0X101, F
-    RRNCF 0X101, W
-    
-    ; SEE IF THE TEH NUMBER IS BIGGER THAN 5
-    
-    SUBLW 5
-    BN BIGGER_THAN_5
-    BRA SMALLER_THAN_5
-    
-BIGGER_THAN_5    ; CONDITION IS MET
-    MOVLW B'00000000'      ; TURNING OFF TRANSISTOR Q3
-    MOVWF LATA
-    
-    MOVLW B'00000110'
-    MOVWF LATF
-    
-    MOVLW B'11111110'      ; SETTING TRANSISTOR Q2 AS OUTPUT TO TURN OFF
-    MOVWF LATH
-    
-SMALLER_THAN_5
-NUMBER_IS_NEGATIVE
-    BRA L_MAIN
-    
-    end
-    
+    end ; do not forget the end statement!
