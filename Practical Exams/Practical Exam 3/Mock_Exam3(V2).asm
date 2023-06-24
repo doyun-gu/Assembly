92fd036e04b449e024b7378816303cdeb83bf30c
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-25 00:03:27 +0900

    Update Mock_Exam3(V2).asm

--- Practical Exams/Practical Exam 3/Mock_Exam3(V2).asm
+++ Practical Exams/Practical Exam 3/Mock_Exam3(V2).asm
@@ -30,18 +30,18 @@ Switches equ 0x03
 
 ;setup TRIS - non used pins set to input to protect pins
 
-    MOVLW B'00000000';for anything all output
+    MOVLW B'00000000'    ;for anything all output
     MOVWF TRISF
  
-    MOVLW B'11111111' ; Anything all input
+    MOVLW B'11111111'    ; Anything all input
     MOVWF TRISC
     MOVWF TRISB
     MOVWF TRISJ
  
-    MOVLW B'11110000'; First pins for transistors, last for switches
+    MOVLW B'11110000'    ; First pins for transistors, last for switches
     MOVWF TRISH
  
-    MOVLW B'11101111';For controlling LED transistor - RA4 
+    MOVLW B'11101111'    ;For controlling LED transistor - RA4 
     MOVWF TRISA
     
     MOVLB 4; Set BSR
@@ -53,65 +53,65 @@ Main
     MOVLW B'11111111'
     MOVWF LATF;set data - all LEDs will be on
     
-    MOVLW B'000000011';both segments off - does not need changing.
+    MOVLW B'000000011'     ;both segments off - does not need changing.
     MOVWF LATH
     
-    MOVLW B'00010000';leds on
+    MOVLW B'00010000'      ;leds on
     MOVWF LATA; RA4 set to high
     
-    BTFSS Switches, 5;if switch pressed
+    BTFSS Switches, 5      ;if switch pressed
     CALL DelaySub400ms;400ms
     
-    BTFSC Switches, 5; if switch not pressed
-    CALL DelaySub40ms;delay 40ms
+    BTFSC Switches, 5      ; if switch not pressed
+    CALL DelaySub40ms      ;delay 40ms
     
-    MOVLW B'00000000';leds off
-    MOVWF LATA; RA4 set to low
+    MOVLW B'00000000'      ;leds off
+    MOVWF LATA             ; RA4 set to low
     
-    BTFSS Switches, 5;if switch pressed
-    CALL DelaySub400ms;400ms
+    BTFSS Switches, 5      ;if switch pressed
+    CALL DelaySub400ms     ;400ms
     
-    BTFSC Switches, 5; if switch not pressed
-    CALL DelaySub40ms;delay 40ms
+    BTFSC Switches, 5      ; if switch not pressed
+    CALL DelaySub40ms      ;delay 40ms
     
     BRA Main; loop to start
     
 Delay_Sub_200us
     MOVLW 71
-    MOVWF DelayCounter1,1;set counter, BSR used
+    MOVWF DelayCounter1,1   ;set counter, BSR used
 
 InnerLoop1
     NOP;waste a load of time
     NOP
     NOP
     NOP
-    DECF DelayCounter1,1,1 ; decrease counter, BSR used 
-    BNZ InnerLoop1;if counter not yet zero, loop
+    DECF DelayCounter1,1,1  ; decrease counter, BSR used 
+    BNZ InnerLoop1          ;if counter not yet zero, loop
     RETURN
     
 DelaySub40ms
-    MOVLW 200;40ms/200us = 200 iterations
+    MOVLW 200               ;40ms/200us = 200 iterations
     MOVWF DelayC2
 
 InnerLoop2
-    CALL Delay_Sub_200us;call 200us delay
-    DECF DelayC2; decrease counter
-    BNZ InnerLoop2;if counter not yet zero, loop
+    CALL Delay_Sub_200us    ;call 200us delay
+    DECF DelayC2            ; decrease counter
+    BNZ InnerLoop2          ;if counter not yet zero, loop
     RETURN   
     
 DelaySub400ms
-    MOVLW 10;overflows if using 2000x200us, so instead use 10x40ms
+    MOVLW 10                ;overflows if using 2000x200us, so instead use 10x40ms
     MOVWF DelayC3
 
 InnerLoop3
-    CALL DelaySub40ms;call 40ms delay 
-    DECF DelayC3; decrease counter
-    BNZ InnerLoop3;if counter not yet zero, loop
+    CALL DelaySub40ms       ;call 40ms delay 
+    DECF DelayC3            ; decrease counter
+    BNZ InnerLoop3          ;if counter not yet zero, loop
     RETURN
     
 GetSwitch; Reads PB1
-    MOVF PORTJ, W; read in PortJ for PB1
-    ANDLW B'00100000';only J5 needed
+    MOVF PORTJ, W          ; read in PortJ for PB1
+    ANDLW B'00100000'      ;only J5 needed
     MOVWF Switches
     RETURN
     
